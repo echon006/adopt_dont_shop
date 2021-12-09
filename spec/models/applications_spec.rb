2e035db283dbd912d3505f65b8eac25f2a387d51
Eric Chon <echon92@gmail.com>
2021-12-09 15:58:09 -0700

    created joined table test

--- spec/models/applications_spec.rb
+++ spec/models/applications_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Application, type: model do
+RSpec.describe Application, type: :model do
   describe "relationships" do
     it { should have_many :application_pets}
   end
