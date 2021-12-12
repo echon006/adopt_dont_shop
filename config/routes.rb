0d5219999a75a25be6ef49c467b5e9de64f521f6
Eric Chon <echon92@gmail.com>
2021-12-12 10:34:33 -0700

    application show test passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
-  
+  get '/applications/:id', to: 'application#show'
+
 
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+applications = [
+  ["Applicant 1", "555 Turing st", "Aurora", "CO", "80016"],
+  ["Applicant 2", "123 Turing st", "Denver", "CO", "80016"],
+  ["Applicant 3", "777 Turing st", "Boulder", "CO", "80016"]
+]
+
+applications.each do |name, street_address, city, state, zip_code|
+  Application.create(name: name , street_address: street_address, city: city, state: state, zip_code: zip_code)
+end
